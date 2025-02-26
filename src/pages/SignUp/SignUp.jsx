146e2094c14b9ff541651c06edefde92d0b64288
JeremyOroc <2034933@johnabbottcollege.net>
2025-02-26 13:50:14 -0500

    Implemented email/password sign up using firebase authentication

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { useCookies } from 'react-cookie';
-import { handleCookies } from '../../utils/helpers';
-import './SignUp.css';
-
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { handleCookies } from "../../utils/helpers";
+import "./SignUp.css";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 export default function SignUp() {
   const [name, setName] = useState("");
@@ -16,9 +16,18 @@ export default function SignUp() {
 
   async function onSignUp() {
     try {
-      const { data } = await axios.post('https://jsonplaceholder.typicode.com/posts', {
-        name, email, password
-      });
+      const { data } = await axios.post(
+        "https://jsonplaceholder.typicode.com/posts",
+        {
+          name,
+          email,
+          password,
+        }
+      );
+
+      // create user in firebase authentication
+      const auth = getAuth();
+      createUserWithEmailAndPassword(auth, email, password);
 
       if (data) {
         setCookie("idCookie", data.name, { path: "/" });
@@ -28,32 +37,48 @@ export default function SignUp() {
         navigate("/");
       }
     } catch (ex) {
-      setError(ex.response.data.error || 'Whoops! Something went wrong ');
+      setError(ex.response.data.error || "Whoops! Something went wrong ");
     }
   }
 
   return (
-
     <div className="signup_wrapper">
       {error && <h1>{error}</h1>}
-      <div className='boxS'>
+      <div className="boxS">
         <h1>Sign Up</h1>
         <label>
           <h4>Username:</h4>
-          <input className='forms' type="text" name="name" onChange={(ev) => setName(ev.target.value)} />
+          <input
+            className="forms"
+            type="text"
+            name="name"
+            onChange={(ev) => setName(ev.target.value)}
+          />
         </label>
         <label>
           <h4>Email:</h4>
-          <input className='forms' type="text" name="email" onChange={(ev) => setEmail(ev.target.value)} />
+          <input
+            className="forms"
+            type="text"
+            name="email"
+            onChange={(ev) => setEmail(ev.target.value)}
+          />
         </label>
         <label>
           <h4>Password:</h4>
-          <input className='forms' type="password" name="password" onChange={(ev) => setPassword(ev.target.value)} />
+          <input
+            className="forms"
+            type="password"
+            name="password"
+            onChange={(ev) => setPassword(ev.target.value)}
+          />
         </label>
         <div>
-          <button className='user_btn' type="submit" onClick={onSignUp}>Register</button>
+          <button className="user_btn" type="submit" onClick={onSignUp}>
+            Register
+          </button>
         </div>
       </div>
     </div>
   );
-};
+}
