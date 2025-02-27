76618c5de0fa2a9c1f81cb66dfda62aaa820b754
Nicolasgharzouzi <Nicolasgharzouzi04@gmail.com>
2025-02-27 00:50:49 -0500

    Update MessageList.js (connect to websockets)

--- UI design direct-messaging/src/containers/message/MessageList.js
+++ UI design direct-messaging/src/containers/message/MessageList.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
+import { connect, useDispatch } from 'react-redux'; 
 import io from 'socket.io-client';
-import socketIOClient from "socket.io-client";
 import { messagesRequested, newMessageAdded } from '../../store/actions';
 import Message from '../../components/message/Message';
 import './MessageList.scss';
@@ -9,65 +8,46 @@ import './MessageList.scss';
 const socket = io("http://localhost:3002"); 
 
 const MessageList = ({ conversationId, getMessagesForConversation, loadMessages }) => {
+    const dispatch = useDispatch();  
+
     const messageDetails = getMessagesForConversation(conversationId);
-    const messages = messageDetails ? messageDetails.messages: null;
-    let messageItems = null;
+    const messages = messageDetails ? messageDetails.messages : [];
 
     useEffect(() => {
         if (!messageDetails) {
             loadMessages(conversationId, null);
         }
-    }, [messageDetails, loadMessages, conversationId])
 
-    if (messages && messages.length > 0) {
-        messageItems = messages.map((message, index) => {
-            return <Message 
-                key={index}
-                isMyMessage={message.isMyMessage}
-                message={message} />;
+       
+        socket.on("message", (message) => {
+            console.log("Received message:", message); 
+            if (message.conversationId === conversationId) {
+                dispatch(newMessageAdded(conversationId, message));  
+            }
         });
-    }
+
+        return () => {
+            socket.off("message");  
+        };
+    }, [messageDetails, loadMessages, conversationId, dispatch]);
 
     return (
         <div id="chat-message-list">
-            {messageItems}
+            {messages.length > 0 ? messages.map((message, index) => (
+                <Message key={index} isMyMessage={message.isMyMessage} message={message} />
+            )) : <p>No messages yet</p>}
         </div>
     );
-}
+};
 
-const mapStateToProps = state => {
-    const getMessagesForConversation = conversationId => {
-        return state.messagesState.messageDetails[conversationId];
-    }
+const mapStateToProps = state => ({
+    getMessagesForConversation: conversationId => state.messagesState.messageDetails[conversationId]
+});
 
-    return {
-        getMessagesForConversation
-    }
-}
+const mapDispatchToProps = dispatch => ({
+    loadMessages: (conversationId, lastMessageId) => dispatch(messagesRequested(conversationId, 5, lastMessageId))
+});
 
-const mapDispatchToProps = dispatch => {
-    const loadMessages = (conversationId, lastMessageId) => {
-        dispatch(messagesRequested(conversationId, 5, lastMessageId));
-    }
+export default connect(mapStateToProps, mapDispatchToProps)(MessageList);
 
-    return { loadMessages };
-}
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(MessageList);
-
-function MyComponent({ dispatch }) {
-    
-  const sendMessage = (conversationId, messageContent) => {
-    const messages = {
-        id: new Date().getTime(), 
-        messageText: messageContent,
-        isMyMessage: true,
-        createdAt: new Date().toISOString(),
-    };
-
-    dispatch(newMessageAdded(conversationId, messages));
-    socket.emit('newMessage', { conversationId, messages }); 
-}};
