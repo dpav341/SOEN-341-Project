4d3a72f395026641c6ffc56f5569cbdca1c19a95
dpav341 <d.pavone@outlook.com>
2025-02-27 14:05:45 -0500

    Update db.js to ES6

--- server/db.js
+++ server/db.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose'
-export const getRoomCollection = (room) => mongoose.connection.collection(`room_chat_${room}`);
+
+// export const getRoomCollection = (room) => mongoose.connection.collection(`room_chat_${room}`);
+
 export function Connection() {
     const mongoURI = "mongodb+srv://soen341projectw2025:<password>@skigo-cluster.fduxd.mongodb.net/?retryWrites=true&w=majority&appName=SkiGo-Cluster"
     mongoose.connect(mongoURI)
-    .then(() => console.log("connected"))
+    .then(() => console.log("Connected to MongoDB."))
     .catch(err => console.log(err))
 }
