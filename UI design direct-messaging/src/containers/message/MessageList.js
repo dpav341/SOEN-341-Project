f2e42feba24049abb49b7bc1afc61a23e7c6a535
Nicolasgharzouzi <Nicolasgharzouzi04@gmail.com>
2025-02-27 00:02:22 -0500

    Update MessageList.js

--- UI design direct-messaging/src/containers/message/MessageList.js
+++ UI design direct-messaging/src/containers/message/MessageList.js
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-
-import { messagesRequested } from '../../store/actions';
+import io from 'socket.io-client';
+import socketIOClient from "socket.io-client";
+import { messagesRequested, newMessageAdded } from '../../store/actions';
 import Message from '../../components/message/Message';
 import './MessageList.scss';
+
+const socket = io("http://localhost:3002"); 
 
 const MessageList = ({ conversationId, getMessagesForConversation, loadMessages }) => {
     const messageDetails = getMessagesForConversation(conversationId);
@@ -54,3 +57,17 @@ export default connect(
     mapStateToProps,
     mapDispatchToProps
 )(MessageList);
+
+function MyComponent({ dispatch }) {
+    
+  const sendMessage = (conversationId, messageContent) => {
+    const messages = {
+        id: new Date().getTime(), 
+        messageText: messageContent,
+        isMyMessage: true,
+        createdAt: new Date().toISOString(),
+    };
+
+    dispatch(newMessageAdded(conversationId, messages));
+    socket.emit('newMessage', { conversationId, messages }); 
+}};
