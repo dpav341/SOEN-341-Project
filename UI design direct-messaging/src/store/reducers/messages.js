ba23d41d9d99a53caa3241c49b7ad714509f1c87
Nicolasgharzouzi <Nicolasgharzouzi04@gmail.com>
2025-02-26 23:55:26 -0500

    Update messages.js

--- UI design direct-messaging/src/store/reducers/messages.js
+++ UI design direct-messaging/src/store/reducers/messages.js
@@ -2,26 +2,39 @@ const initialState = {
     messageDetails: {}
 }
 
-const messagesReducer = (state = initialState, action) => {
-    switch(action.type) {
+const messagesReducer = (state = { messageDetails: {} }, action) => {
+    switch (action.type) {
         case 'MESSAGES_LOADED':
-            const { conversationId, messages, hasMoreMessages, lastMessageId } = action.payload;
-            const currentConversationMapEntry = state.messageDetails[conversationId];
-            const newConversationMapEntry = { hasMoreMessages, lastMessageId, messages: [] };
-
-            if (currentConversationMapEntry) {
-                newConversationMapEntry.messages = [...currentConversationMapEntry.messages];
-            }
-
-            newConversationMapEntry.messages = [...newConversationMapEntry.messages, ...messages];
-
-            const newMessageDetails = { ...state.messageDetails };
-            newMessageDetails[conversationId] = newConversationMapEntry;
-
-            return { messageDetails: newMessageDetails };
-        default: 
+            
+            return {
+                ...state,
+                messageDetails: {
+                    ...state.messageDetails,
+                    [action.payload.conversationId]: {
+                        ...state.messageDetails[action.payload.conversationId],
+                        messages: action.payload.messages,
+                        hasMoreMessages: action.payload.hasMoreMessages,
+                        lastMessageId: action.payload.lastMessageId
+                    }
+                }
+            };
+        case 'NEW_MESSAGE_ADDED':
+            
+            const { conversationId, message } = action.payload;
+            const existingMessages = state.messageDetails[conversationId]?.messages || [];
+            return {
+                ...state,
+                messageDetails: {
+                    ...state.messageDetails,
+                    [conversationId]: {
+                        ...state.messageDetails[conversationId],
+                        messages: [...existingMessages, message]
+                    }
+                }
+            };
+        default:
             return state;
     }
-}
+};
 
 export default messagesReducer;
