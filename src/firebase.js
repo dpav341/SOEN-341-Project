07462f987500c942aa645009896acf68a4114531
Hawad Ahmad <hawadjan.1@gmail.com>
2025-02-27 00:23:43 -0500

    use .env variables

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,13 @@ import { getDatabase } from 'firebase/database';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCzvbbPQ_Ohn4DqgN0PVOAjtFNO6-pKK-w",
-    authDomain: "soen-341-2c6ec.firebaseapp.com",
-    projectId: "soen-341-2c6ec",
-    storageBucket: "soen-341-2c6ec.firebasestorage.app",
-    messagingSenderId: "431920145168",
-    appId: "1:431920145168:web:a6ac5e4aedc4e819133b95",
-    measurementId: "G-KF5P9S74JJ"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
