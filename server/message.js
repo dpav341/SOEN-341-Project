e9b02f3cca820bd33aea92b366b1b49917ade678
dpav341 <d.pavone@outlook.com>
2025-02-28 16:39:48 -0500

    Update document field

--- server/message.js
+++ server/message.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const messageSchema = new mongoose.Schema({
     username: String,
-    content: String,
+    message: String,
     timestamp: {type: Date, default: Date.now},
 })
 
