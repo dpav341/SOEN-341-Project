80a56ec5bc9af2eb35f42ae0a7072ff1b0a20f3e
dpav341 <d.pavone@outlook.com>
2025-03-01 22:16:48 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
+
+let users = [];
+
+const messages = {
+    general: []
+};
+
+io.on('connection', socket => {
+    socket.on("join server", (username) => {
+        const user = {
+            username,
+            id: socket.id,
+        }
+        users.push(user);
+        io.emit("new user", users);
+    });
+
+    socket.on("join room", (roomName, cb) => {
+        socket.join(roomName);
+        cb(messages[roomName]);
+    });
+
+    socket.on("send message", ({content, to, sender, chatName, isChannel}) => {
+        if (isChannel) {
+            const payload = {
+                content,
+                chatName,
+                sender,
+            };
+            socket.to(to).emit("new message", payload);
+        }
+        else {
+            const payload = {
+                content,
+                chatName : sender,
+                sender,
+            };
+            socket.to(to).emit("new message", payload);
+        }
+        if (messages[chatName]) {
+            messages[chatName].push({
+                sender,
+                content
+            });
+        }
+    });
+
+    socket.on("disconnect", () => {
+        users = users.filter(u => u.id !== socket.id);
+        io.emit("new user", users);
+    });
+});
+
+server.listen(1337, () => console.log('Server is running on port 1337'));
