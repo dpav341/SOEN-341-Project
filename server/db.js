a93d3794c0fd8d305ed8e880f1d3f6be7320d06c
dpav341 <d.pavone@outlook.com>
2025-03-13 09:40:05 -0400

    Update db.js with process.env.MONGODB_PASSWORD

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 export function Connection() {
-    const mongoURI = `mongodb+srv://soen341projectw2025:<password>@skigo-cluster.fduxd.mongodb.net/?retryWrites=true&w=majority&appName=SkiGo-Cluster`
+    const mongoURI = `mongodb+srv://soen341projectw2025:process.env.MONGODB_PASSWORD@skigo-cluster.fduxd.mongodb.net/?retryWrites=true&w=majority&appName=SkiGo-Cluster`
     mongoose.connect(mongoURI)
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.log(err))
