fe01b68810db6b43c4c312917146b214b2981199
dpav341 <d.pavone@outlook.com>
2025-03-12 22:23:44 -0400

    Create db.js

---
+++ server/db.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose'
+
+export function Connection() {
+    const mongoURI = `mongodb+srv://soen341projectw2025:mernstack@skigo-cluster.fduxd.mongodb.net/?retryWrites=true&w=majority&appName=SkiGo-Cluster`
+    mongoose.connect(mongoURI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.log(err))
+}
