1087b32be395c5cd320d0cb338c5e5a81443e69b
Hawad Ahmad <hawadjan.1@gmail.com>
2025-03-22 17:59:09 -0400

    remove unused imports

--- server/index.js
+++ server/index.js
@@ -1,21 +1,14 @@
 import express from 'express'
 import { Server } from "socket.io"
-import path from 'path'
-import { fileURLToPath } from 'url'
 import { getChannelCollection, getDMCollection, saveMessage, Conversation } from './message.js'
 import { config } from 'dotenv'
 config({ path: '../.env' })
 import { Connection } from './db.js'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
 const PORT = process.env.PORT || 3002
 const ADMIN = "Admin"
 
 const app = express()
-
-app.use(express.static(path.join(__dirname, "public")))
 
 const expressServer = app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
@@ -194,7 +187,5 @@ function getAllActiveRooms() {
 
 io.on("connection", (socket) => {
     console.log("connected");
-
-
 })
 
