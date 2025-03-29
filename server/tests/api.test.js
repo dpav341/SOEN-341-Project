d78351bf76f7b2d6bfe9ee874261cbde109d2a39
Hawad Ahmad <hawadjan.1@gmail.com>
2025-03-29 13:24:48 -0400

    add test case for updated user list inside room when someone leaves

--- server/tests/api.test.js
+++ server/tests/api.test.js
@@ -1,18 +1,23 @@
 import { io as Client } from "socket.io-client";
 import { expressServer } from "../index.js";
 
-let clientSocket;
+let clientSocket1, clientSocket2;
 
 beforeEach(async () => {
-    clientSocket = new Client("http://localhost:3002");
-    await new Promise((resolve) => {
-        clientSocket.on("connect", resolve);
-    });
+    clientSocket1 = new Client("http://localhost:3002");
+    clientSocket2 = new Client("http://localhost:3002");
+    await Promise.all([
+        new Promise((resolve) => clientSocket1.on("connect", resolve)),
+        new Promise((resolve) => clientSocket2.on("connect", resolve)),
+    ]);
 });
 
 afterEach(() => {
-    if (clientSocket.connected) {
-        clientSocket.disconnect();
+    if (clientSocket1.connected) {
+        clientSocket1.disconnect();
+    }
+    if (clientSocket2.connected) {
+        clientSocket2.disconnect();
     }
 });
 
@@ -25,15 +30,16 @@ afterAll((done) => {
 
 describe("Socket.IO Server", () => {
     test("should establish a socket connection", async () => {
-        expect(clientSocket.connected).toBe(true);
+        expect(clientSocket1.connected).toBe(true);
+        expect(clientSocket2.connected).toBe(true);
     });
 
     test("should emit and receive a 'newMessage' event", (done) => {
         const testMessage = { name: "User2", text: "New Message" };
 
-        clientSocket.emit("newMessage", testMessage);
+        clientSocket1.emit("newMessage", testMessage);
 
-        clientSocket.on("message", (msg) => {
+        clientSocket1.on("message", (msg) => {
             expect(msg).toEqual(expect.objectContaining(testMessage));
             done();
         });
@@ -42,35 +48,65 @@ describe("Socket.IO Server", () => {
     test("should handle 'enterRoom' event and emit 'messageList'", (done) => {
         const testRoom = { name: "User3", room: "TestRoom" };
 
-        clientSocket.emit("enterRoom", testRoom);
+        clientSocket1.emit("enterRoom", testRoom);
 
-        clientSocket.on("messageList", (messages) => {
+        clientSocket1.on("messageList", (messages) => {
             expect(messages).toEqual([]);
             done();
         });
     });
 
     test("should handle 'disconnect' event", (done) => {
-        clientSocket.on("disconnect", () => {
-            expect(clientSocket.connected).toBe(false);
+        clientSocket1.on("disconnect", () => {
+            expect(clientSocket1.connected).toBe(false);
             done();
         });
 
-        clientSocket.disconnect();
+        clientSocket1.disconnect();
     });
 
     test("should send the list of users inside a room", (done) => {
         const testRoom = { name: "User1", room: "TestRoom" };
 
-        clientSocket.emit("enterRoom", testRoom);
+        clientSocket1.emit("enterRoom", testRoom);
 
-        clientSocket.on("userList", (data) => {
+        clientSocket1.on("userList", (data) => {
             expect(data).toEqual({
                 users: expect.arrayContaining([
                     expect.objectContaining({ name: "User1", room: "TestRoom" }),
                 ]),
             });
             done();
+        });
+    });
+
+    test("should send the updated user list when someone leaves the room", (done) => {
+        let count = 0;
+        const testRoom1 = { name: "User1", room: "TestRoom" };
+        const testRoom2 = { name: "User2", room: "TestRoom" };
+
+        clientSocket1.emit("enterRoom", testRoom1);
+        clientSocket2.emit("enterRoom", testRoom2);
+
+        clientSocket1.on("userList", (data) => {
+            expect(data).toEqual({
+                users: expect.arrayContaining([
+                    expect.objectContaining({ name: "User1", room: "TestRoom" }),
+                ]),
+            });
+            expect(data.users).not.toEqual(
+                expect.arrayContaining([
+                    expect.objectContaining({ name: "User2", room: "TestRoom" }),
+                ])
+            );
+            count++;
+            if (count >= 1)
+                done();
+        });
+
+        clientSocket2.emit("enterRoom", {
+            name: "User2",
+            room: "anotherRoom",
         });
     });
 });
