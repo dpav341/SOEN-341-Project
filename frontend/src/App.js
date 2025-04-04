f877b6d6d90faa4ad5b51c5087421e7e073247c7
Nicolasgharzouzi <Nicolasgharzouzi04@gmail.com>
2025-04-04 19:37:22 -0400

    Updated code

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Weather from './weather/weather.js'; // Import Weather component from weather.js
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
+
